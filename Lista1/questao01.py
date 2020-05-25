5d417c4099f06c0677fa454b94067b54099f3017
Lisandro Guerra <lisandro.digital@gmail.com>
2020-05-25 20:08:12 -0300

    Update questao01.py

--- Lista1/questao01.py
+++ Lista1/questao01.py
@@ -1,4 +1,4 @@
 print('Cálculo da soma entre 2 números')
-n1 = int(input("Entre com o primeiro número: "))
-n2 = int(input("Entre com o segundo número: "))
-print("A soma de", n1, "com", n2, "é igual a", n1+n2)
+number1 = int(input("Entre com o primeiro número: "))
+number2 = int(input("Entre com o segundo número: "))
+print("A soma de", number1, "com", number2, "é igual a", number1 + number2)
