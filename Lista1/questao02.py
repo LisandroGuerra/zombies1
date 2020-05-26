a5110ddb1c3d798013aded11d4ca630ecb2a0f40
Lisandro Guerra <lisandro.digital@gmail.com>
2020-05-26 07:57:00 -0300

    Update questao02.py

--- Lista1/questao02.py
+++ Lista1/questao02.py
@@ -1,3 +1,3 @@
 print('Conversão de metros em milímetros')
-n1 = float(input("Entre com a distância em metros: "))
-print(n1, "metros equivale a", int(n1*1000), "milímetros")
+number1 = float(input("Entre com a distância em metros: "))
+print(number1, "metros equivale a", int(number1*1000), "milímetros")
