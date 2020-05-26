48e7615c75b89fe29ac4b0f2439e1724de91714f
Lisandro Guerra <lisandro.digital@gmail.com>
2020-05-26 07:58:53 -0300

    Update questao03.py

--- Lista1/questao03.py
+++ Lista1/questao03.py
@@ -3,5 +3,5 @@ dias = int(input('Entre com o número de dias: '))
 horas = int(input('Entre com o número de horas: '))
 minutos = int(input('Entre com o número de minutos: '))
 segundos = int(input('Entre com o número de segundos: '))
-total = (dias*24*60*60)+(horas*60+minutos)*60+segundos
+total = (dias * 24 * 60 * 60) + (horas * 60 + minutos) * 60 + segundos
 print('O total em segundos é:', total)
