5392be9d098f5417803037ce329621423efe5dab
Lisandro Guerra <lisandro.digital@gmail.com>
2020-05-26 07:58:03 -0300

    Update questao04.py

--- Lista1/questao04.py
+++ Lista1/questao04.py
@@ -1,7 +1,7 @@
 print('Calcula o valor de um salário depois de um aumento percentual')
 salario = float(input("Entre com o salário(ex:788.00: R$ "))
 percentual = int(input("Entre com o percentual(ex:10: "))
-final = salario+salario*percentual/100
+final = salario + salario * percentual / 100
 print('Salário inicial: R$', "%.2f" %salario)
 print('Percentual:', percentual, '%')
 print('Salário final: R$', "%.2f" %final)
