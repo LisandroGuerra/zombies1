198e294d1f04576cb8a6663b69c675b2972f15a7
Lisandro Guerra <lisandro.digital@gmail.com>
2020-05-27 08:01:49 -0300

    Update questao08.py

--- Lista1/questao08.py
+++ Lista1/questao08.py
@@ -1,4 +1,4 @@
 print('Converte Fahrenheit para Celcius')
-f = float(input('Entre com a temperatura em F: '))
+f = float(input('Entre com a temperatura em Fahrenheit: '))
 c = (f - 32) / 9 * 5
 print('A temperatura é de', '%.2f' %c,'ºC')
