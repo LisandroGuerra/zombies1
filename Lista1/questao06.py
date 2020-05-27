8aea984ecf64ad4d671961f58835d0ea7bcf8aa1
Lisandro Guerra <lisandro.digital@gmail.com>
2020-05-27 07:59:28 -0300

    Update questao06.py

--- Lista1/questao06.py
+++ Lista1/questao06.py
@@ -1,5 +1,5 @@
 print('Cálculo do tempo de viagem')
 distancia = float(input('Entre com a distância em km: '))
 velocidade = int(input('Entre com a velocidade média em km/h: '))
-tempo = distancia/velocidade
+tempo = distancia / velocidade
 print('O tempo de viagem será de aproximadamente:', '%.2f' %tempo,'horas')
