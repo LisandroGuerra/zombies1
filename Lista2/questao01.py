97f3bdb6c7f8964597aa39eeb31f8b1cc21ef38e
Lisandro Guerra <lisandro.digital@gmail.com>
2020-05-29 07:47:36 -0300

    Update questao01.py

--- Lista2/questao01.py
+++ Lista2/questao01.py
@@ -1,14 +1,14 @@
 print('Qual o tipo de triangulo')
-a = int(input("Entre com o primeiro lado: "))
-b = int(input("Entre com o segundo lado: "))
-c = int(input("Entre com o terceiro lado: "))
-if a > b + c or b > a + c or c > a + b:
+side_a = int(input("Entre com o primeiro lado: "))
+side_b = int(input("Entre com o segundo lado: "))
+side_c = int(input("Entre com o terceiro lado: "))
+if side_a > side_b + side_c or side_b > side_a + side_c or side_c > side_a + side_b:
     print ('Não é um triângulo')
-elif a == 0 or b == 0 or c == 0:
+elif side_a == 0 or side_b == 0 or side_c == 0:
     print('Nenhum lado pode ser zero')
-elif a == b and b == c:
+elif side_a == side_b and side_b == side_c:
     print('É equilátero')
-elif a != b and b != c and a != c:
+elif side_a != side_b and side_b != side_c and side_a != side_c:
     print('É isósceles')
 else:
     print('É escaleno')
