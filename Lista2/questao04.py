00722f8888ee9bdf23c40caa3068b282766de546
Lisandro Guerra <lisandro.digital@gmail.com>
2020-05-29 07:49:25 -0300

    Update questao04.py

--- Lista2/questao04.py
+++ Lista2/questao04.py
@@ -1,10 +1,10 @@
 print('Qual o maior de 3 números')
-n1 = int(input("Entre com o primeiro número: "))
-n2 = int(input("Entre com o segundo número: "))
-n3 = int(input("Entre com o terceiro número: "))
-if n1 >= n2 and n1 >= n3:
-    print('O maior é: %d' %n1)
-elif n2 >= n3:
-    print('O maior é: %d' %n2)
+number1 = int(input("Entre com o primeiro número: "))
+number2 = int(input("Entre com o segundo número: "))
+number3 = int(input("Entre com o terceiro número: "))
+if number1 >= number2 and number1 >= number3:
+    print('O maior é: %d' %number1)
+elif number2 >= number3:
+    print('O maior é: %d' %number2)
 else:
-    print('O maior é: %d' %n3)
+    print('O maior é: %d' %number3)
