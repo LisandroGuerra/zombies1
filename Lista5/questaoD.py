3875ee2849f2aef3392391873e38d8fd569538d0
Lisandro Guerra <lisandro.digital@gmail.com>
2020-05-31 00:23:59 -0300

    Update questaoD.py

--- Lista5/questaoD.py
+++ Lista5/questaoD.py
@@ -1,8 +1,8 @@
-c = 0
+counter = 0
 for i in range(18644, 33088):
     if '2' in str(i) and '7' not in str(i):
-        c += 1
+        counter += 1
 
-print (c)
+print (counter)
 
 # print (len([i for i in range(18644, 33088) if '2' in str(i) and '7' not in str(i)]))
