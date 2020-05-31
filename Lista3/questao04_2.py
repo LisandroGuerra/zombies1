b9f1099e4287fcf70a65af03409f8364b6c57186
Lisandro Guerra <lisandro.digital@gmail.com>
2020-05-30 21:23:14 -0300

    Update questao04_2.py

--- Lista3/questao04_2.py
+++ Lista3/questao04_2.py
@@ -4,7 +4,7 @@ def fibonacci(num):
     if num <= 1:
         return num
     else:
-        return(fibonacci(num-1) + fibonacci(num-2))
+        return(fibonacci(num - 1) + fibonacci(num - 2))
     
 
 num = int(input("Digite um número: "))
