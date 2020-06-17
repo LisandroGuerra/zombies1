9484de4f2ff2e963ab19116b6415066ef2ee54dd
Lisandro Guerra <lisandro.digital@gmail.com>
2020-06-17 08:14:11 -0300

    Update questao02.py

--- Lista3_desafio/questao02.py
+++ Lista3_desafio/questao02.py
@@ -1,7 +1,7 @@
 conta = int(input("Entre com o valor da conta: "))
 pago = int(input("Entre com o valor pago: "))
-troco = pago - conta
 notas = [50, 20, 10, 5, 2, 1]
+troco = pago - conta
 for nota in notas:
     while troco >= nota:
         print("Dar troco com nota de %d" %nota)
